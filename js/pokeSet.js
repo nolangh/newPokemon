befcd65f596aebbb40869e940d43acfb192c9958
nolangh <nolanghiggins@gmail.com>
2023-04-18 18:09:48 -0600

    -a

--- js/pokeSet.js
+++ js/pokeSet.js
@@ -1,75 +1,59 @@
 const apiKey = "ef72570ff371408f9668e414353b7b2e";
 const apiSets = "https://api.pokemontcg.io/v2/sets";
-const card = document.querySelector("card");
 const cardContainer = document.getElementById("card-cont");
 const reset = document.getElementById("reset-btn");
 const pos = document.getElementById("selectSet");
-const pokeCards = []; //this is the array that has each pokemon
 let cardSets = [];
-let allPokemon = [];
-let goodLord = [];
 let setName;
 
-/* -----------------------------ANCHOR Event Listners ----------------------------- */
+/* -----------------------------ANCHOR Event Listeners ----------------------------- */
+
 reset.addEventListener("click", () => {
 	window.location.reload(true);
 });
 
 pos.addEventListener("change", () => {
-	setName = pos.options[pos.selectedIndex].value;
-	return fetch(`https://api.pokemontcg.io/v2/cards?q=set.id:${setName}`, {
-		method: "GET",
+	setName = pos.value;
+	fetch(`https://api.pokemontcg.io/v2/cards?q=set.id:${setName}`, {
 		headers: {
 			"X-api-key": apiKey,
 		},
 	})
-		.then((res) => {
-			return res.json();
-		})
+		.then((res) => res.json())
 		.then((filteredSet) => {
-			allPokemon = filteredSet;
-			console.log(allPokemon);
-		})
-		.then(() => {
-			allPokemon.data.forEach((data) => {
-				createCard(data);
-			});
+			const allPokemon = filteredSet.data;
+			allPokemon.forEach((data) => createCard(data));
 		});
 });
 
 function getSets() {
-	return fetch(apiSets, {
-		method: "GET",
+	fetch(apiSets, {
 		headers: {
 			"X-Api-Key": apiKey,
 		},
 	})
-		.then((res) => {
-			return res.json();
-		})
+		.then((res) => res.json())
 		.then((setInfo) => {
-			cardSets = setInfo;
-		})
-		.then(() => {
+			cardSets = setInfo.data;
 			selectSet();
 		});
 }
 
 function selectSet() {
-	let testSelectHtml = "";
-	cardSets.data.forEach((data) => {
-		testSelectHtml += `<option value="${data.id}">${data.name}</option>`;
-	});
-	document.getElementById("selectSet").innerHTML = testSelectHtml;
+	const testSelectHtml = cardSets.reduce((html, data) => {
+		return html + `<option value="${data.id}">${data.name}</option>`;
+	}, "");
+	pos.innerHTML = testSelectHtml;
 }
 
 function createCard(data) {
 	const pokeCardDiv = document.createElement("div");
 	pokeCardDiv.classList.add("card");
 
-	const pImage = document.createElement("img");
+	const pImage = new Image();
 	pImage.classList.add("card-img-top");
 	pImage.src = data.images.large;
+	pImage.loading = "lazy"; // Add 'loading' attribute for lazy loading
 	pokeCardDiv.appendChild(pImage);
 	cardContainer.appendChild(pokeCardDiv);
 }
